53027e3514af272d6517555a6a9ad6d40f160360
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-06-26 20:54:48 +0900

    add all routings and views

--- wp-content/themes/sample/index.php
+++ wp-content/themes/sample/index.php
@@ -4,8 +4,29 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+// for posts.
+$app->get('/archives/{route}', function ($route) {
+    return renderTemplate(__DIR__ . '/views/single.php');
+})->assert('route', '.*');
+
+// for categories.
+$app->get('/category/{route}', function ($route) {
+    return renderTemplate(__DIR__ . '/views/category.php');
+})->assert('route', '.*');
+
+// for tags.
+$app->get('/tag/{route}', function ($route) {
+    return renderTemplate(__DIR__ . '/views/tag.php');
+})->assert('route', '.*');
+
+// for authors.
+$app->get('/author/{route}', function ($route) {
+    return renderTemplate(__DIR__ . '/views/author.php');
+})->assert('route', '.*');
+
+// default.
 $app->get('/{route}', function ($route) {
-    $path = __DIR__ . '/views/' . ($route ?: 'index') . '.php';
+    $path = __DIR__ . '/views/pages/' . ($route ?: 'index') . '.php';
     if (file_exists($path)) {
         return renderTemplate($path);
     } else {
