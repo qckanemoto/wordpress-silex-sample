38cf71e51b8075aa53b633cc0562ab97a576bc95
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-06-27 12:51:34 +0900

    fix single.php

--- wp-content/themes/sample/views/single.php
+++ wp-content/themes/sample/views/single.php
@@ -1,4 +1,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <h2><?php the_title(); ?></h2>
+    <p>著者：<?php the_author_posts_link(); ?></p>
+    <p>カテゴリ：<?php the_category(); ?></p>
+    <p><?php the_tags(); ?></p>
     <p><?php the_content(); ?></p>
 <?php endwhile; endif; ?>
