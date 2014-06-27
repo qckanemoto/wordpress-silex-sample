2689141ebdc1ff37713e72efc546f26bf62b2f4e
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-06-27 12:52:20 +0900

    fix author.php

--- wp-content/themes/sample/views/author.php
+++ wp-content/themes/sample/views/author.php
@@ -1,1 +1,5 @@
-author
+<h2>著者：<?php the_author(); ?></h2>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <h3><?php the_title(); ?></h3>
+    <p><?php the_content(); ?></p>
+<?php endwhile; endif; ?>
