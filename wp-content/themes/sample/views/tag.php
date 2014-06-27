101c63e049f07e5f7b9b21033e15e80da8033cfa
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-06-27 12:52:05 +0900

    fix tag.php

--- wp-content/themes/sample/views/tag.php
+++ wp-content/themes/sample/views/tag.php
@@ -1,1 +1,5 @@
-tag
+<h2>タグ：<?php single_tag_title(); ?></h2>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <h3><?php the_title(); ?></h3>
+    <p><?php the_content(); ?></p>
+<?php endwhile; endif; ?>
