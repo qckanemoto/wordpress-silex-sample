259506025fd0296b8af6baef7c9a44114af4537b
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-06-27 12:51:50 +0900

    fix category.php

--- wp-content/themes/sample/views/category.php
+++ wp-content/themes/sample/views/category.php
@@ -1,1 +1,5 @@
-category
+<h2>カテゴリ：<?php single_cat_title(); ?></h2>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <h3><?php the_title(); ?></h3>
+    <p><?php the_content(); ?></p>
+<?php endwhile; endif; ?>
